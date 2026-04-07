71c82eab5328ca8c7de74026e0fc88d3eed69ef9
Stanislav Kondratiev <stanislaw.comza@gmail.com>
2026-04-07 22:41:38 +0300

    final MVVM calculator

---
+++ SimpleCalculatorMVVM/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using SimpleCalculatorMVVM.ViewModels;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SimpleCalculatorMVVM
+{
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = (MainViewModel)DataContext;
+
+            // Поддержка клавиатуры
+            this.KeyDown += MainWindow_KeyDown;
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Цифры
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                string digit = e.Key.ToString().Last().ToString();
+                _viewModel.DigitCommand.Execute(digit);
+                e.Handled = true;
+            }
+            else if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                string digit = (e.Key - Key.NumPad0).ToString();
+                _viewModel.DigitCommand.Execute(digit);
+                e.Handled = true;
+            }
+            // Операторы
+            else if (e.Key == Key.Add)
+            {
+                _viewModel.OperatorCommand.Execute("+");
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Subtract)
+            {
+                _viewModel.OperatorCommand.Execute("-");
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Multiply)
+            {
+                _viewModel.OperatorCommand.Execute("x");
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Divide)
+            {
+                _viewModel.OperatorCommand.Execute("÷");
+                e.Handled = true;
+            }
+            // Функции
+            else if (e.Key == Key.Enter)
+            {
+                _viewModel.FunctionCommand.Execute("Equals");
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Decimal || e.Key == Key.OemPeriod)
+            {
+                _viewModel.FunctionCommand.Execute("Decimal");
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                _viewModel.FunctionCommand.Execute("Clear");
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Back)
+            {
+                _viewModel.FunctionCommand.Execute("Delete");
+                e.Handled = true;
+            }
+
+            // Закрываем Popup при нажатии клавиши
+            if (_viewModel.IsScientificPopupOpen)
+                _viewModel.ToggleScientificCommand.Execute(null);
+        }
+    }
+}
---
+++ SimpleCalculatorMVVM/Models/CalculatorModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace SimpleCalculatorMVVM.Models
+{
+    public class CalculatorModel
+    {
+        private string _currentInput = "0";
+        private string _currentOperation = "";
+        private double _firstNumber = 0;
+        private bool _isNewCalculation = true;
+        private bool _isOperatorJustPressed = false;
+        private double _memoryValue = 0;
+
+        public string CurrentInput
+        {
+            get => _currentInput;
+            set => _currentInput = value;
+        }
+
+        public string CurrentOperation
+        {
+            get => _currentOperation;
+            set => _currentOperation = value;
+        }
+
+        public double FirstNumber
+        {
+            get => _firstNumber;
+            set => _firstNumber = value;
+        }
+
+        public bool IsNewCalculation
+        {
+            get => _isNewCalculation;
+            set => _isNewCalculation = value;
+        }
+
+        public bool IsOperatorJustPressed
+        {
+            get => _isOperatorJustPressed;
+            set => _isOperatorJustPressed = value;
+        }
+
+        public double MemoryValue
+        {
+            get => _memoryValue;
+            set => _memoryValue = value;
+        }
+
+        public void CalculateResult()
+        {
+            try
+            {
+                double secondNumber = double.Parse(_currentInput);
+                double result = 0;
+
+                switch (_currentOperation)
+                {
+                    case "+":
+                        result = _firstNumber + secondNumber;
+                        break;
+                    case "-":
+                        result = _firstNumber - secondNumber;
+                        break;
+                    case "x":
+                        result = _firstNumber * secondNumber;
+                        break;
+                    case "÷":
+                        if (secondNumber == 0)
+                            throw new DivideByZeroException();
+                        result = _firstNumber / secondNumber;
+                        break;
+                    case "^":
+                        result = Math.Pow(_firstNumber, secondNumber);
+                        break;
+                }
+
+                _currentInput = result.ToString();
+                _firstNumber = result;
+                _isNewCalculation = true;
+            }
+            catch (DivideByZeroException)
+            {
+                _currentInput = "Ошибка: деление на ноль";
+            }
+            catch (Exception)
+            {
+                _currentInput = "Ошибка";
+                _currentOperation = "";
+                _isNewCalculation = true;
+            }
+        }
+
+        public void Reset()
+        {
+            _currentInput = "0";
+            _currentOperation = "";
+            _firstNumber = 0;
+            _isNewCalculation = true;
+            _isOperatorJustPressed = false;
+        }
+    }
+}
